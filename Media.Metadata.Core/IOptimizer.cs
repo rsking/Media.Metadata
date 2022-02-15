bdff860c95d76da626047ca994c9ad0eea0bd060
Ross King <rsking@gmail.com>
2022-02-15 14:46:48 +1100

    feat: add optimizer

---
+++ Media.Metadata.Core/IOptimizer.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IOptimizer.cs" company="RossKing">
+// Copyright (c) RossKing. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Media.Metadata;
+
+/// <summary>
+/// The optimizer.
+/// </summary>
+public interface IOptimizer
+{
+    /// <summary>
+    /// Optimizes the specified path.
+    /// </summary>
+    /// <param name="path">The path.</param>
+    /// <returns>Whether the file was optimized or not.</returns>
+    public bool Opimize(string path);
+}
---
+++ Media.Metadata.Mp4v2/Mp4Optimizer.cs
@@ -0,0 +1,17 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Mp4Optimizer.cs" company="RossKing">
+// Copyright (c) RossKing. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Media.Metadata;
+using System.Text;
+
+/// <summary>
+/// The MP4v2 optimizer.
+/// </summary>
+internal class Mp4Optimizer : IOptimizer
+{
+    /// <inheritdoc/>
+    public bool Opimize(string path) => NativeMethods.MP4Optimize(Encoding.UTF8.GetBytes(path), newName: null);
+}
