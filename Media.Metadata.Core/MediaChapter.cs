c0ccce7398148b65f24bfbabca3932d7f4f6e2e4
Ross King <rsking@gmail.com>
2022-09-20 07:44:39 +1000

    docs: add XML docs to one method

--- Media.Metadata.Core/MediaChapter.cs
+++ Media.Metadata.Core/MediaChapter.cs
@@ -6,4 +6,9 @@
 
 namespace Media.Metadata;
 
+/// <summary>
+/// The media chapter.
+/// </summary>
+/// <param name="Title">The title.</param>
+/// <param name="Duration">The duration.</param>
 public record class MediaChapter(string Title, TimeSpan Duration);
